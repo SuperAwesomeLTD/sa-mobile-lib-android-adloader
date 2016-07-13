440b65a9aaa4a04db527feaf60c7a41c6e0ebe6a
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-07-13 11:33:02 +0100

    0.2.5

--- saadloader/src/main/java/tv/superawesome/lib/saadloader/SAHTMLParser.java
+++ saadloader/src/main/java/tv/superawesome/lib/saadloader/SAHTMLParser.java
@@ -23,26 +23,53 @@ import tv.superawesome.lib.samodelspace.SAAd;
  */
 public class SAHTMLParser {
 
-    private static final String imageHTML = "<!DOCTYPE html><html><head>\n" +
-            "    <meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/><title>SuperAwesome Image Template</title>\n" +
-            "    <style>\n" +
-            "        html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }\n" +
-            "        img#sa_image { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }\n" +
-            "    </style></head><body><a href='hrefURL' target=\"_blank\"><img id='sa_image' src='imageURL'/></a></body></html>";
-    private static final String richMediaHTML = "<!DOCTYPE html><html><head>\n" +
-            "    <meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/><title>SuperAwesome Rich Media Template</title>\n" +
-            "    <style>\n" +
-            "        html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }\n" +
-            "        iframe { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }\n" +
-            "    </style>\n" +
-            "</head><body><iframe src='richMediaURL'></iframe></body></html>";
-    private static final String tagHTML = "<!DOCTYPE html><html><head>\n" +
-            "    <meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/><title>SuperAwesome 3rd Party Tag Template</title>\n" +
-            "    <style>\n" +
-            "        html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }\n" +
-            "        div#inner { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }\n" +
-            "    </style>\n" +
-            "</head><body><div id=\"inner\">tagdata</div></body></html>";
+    private static final String imageHTML = "" +
+            "<!DOCTYPE html><html>" +
+            "<head>" +
+            "<meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/>" +
+            "<title>SuperAwesome Image Template</title>" +
+            "<style>" +
+            "html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }" +
+            "img#sa_image { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }" +
+            "</style>" +
+            "</head>" +
+            "<body>" +
+            "<a href='hrefURL' target=\"_blank\"><img id='sa_image' src='imageURL'/></a>" +
+            "_MOAT_" +
+            "</body>" +
+            "</html>";
+
+    private static final String richMediaHTML = "" +
+            "<!DOCTYPE html><html>" +
+            "<head>" +
+            "<meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/>" +
+            "<title>SuperAwesome Rich Media Template</title>" +
+            "<style>" +
+            "html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }" +
+            "iframe { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }" +
+            "</style>" +
+            "</head>" +
+            "<body>" +
+            "<iframe src='richMediaURL'></iframe>" +
+            "_MOAT_" +
+            "</body>" +
+            "</html>";
+
+    private static final String tagHTML = "" +
+            "<!DOCTYPE html><html>" +
+            "<head>" +
+            "<meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no\"/>" +
+            "<title>SuperAwesome 3rd Party Tag Template</title>" +
+            "<style>" +
+            "html, body { box-sizing: border-box; width: 100%; height: 100%; padding: 0px; margin: 0px; overflow: hidden; }" +
+            "div#inner { width: _WIDTH_px; height: _HEIGHT_px; border: 0; -webkit-transform-origin: 0 0; -webkit-transform: scale(_SCALE_); position:absolute; }" +
+            "</style>" +
+            "</head>" +
+            "<body>" +
+            "<div id=\"inner\">tagdata</div>" +
+            "_MOAT_" +
+            "</body>" +
+            "</html>";
 
     /**
      * This static function uses ad data to return a formatted HTML string
