73a8bc3dd10202aba637ef2cb74b42ebdc9e7954
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-08-25 18:19:37 +0100

    Updates to simplify code & add the vast parser in

--- saadloader/src/main/java/tv/superawesome/lib/saadloader/SALoader.java
+++ saadloader/src/main/java/tv/superawesome/lib/saadloader/SALoader.java
@@ -1,136 +1,120 @@
-///**
-// * @class: SALoader.java
-// * @copyright: (c) 2015 SuperAwesome Ltd. All rights reserved.
-// * @author: Gabriel Coman
-// * @date: 28/09/2015
-// *
-// */
-//package tv.superawesome.lib.saadloader;
-//
-///**
-// * Imports needed for this implementation
-// */
-//
-//import android.content.Context;
-//import android.util.Log;
-//
-//import org.json.JSONObject;
-//
-//import java.util.Locale;
-//
-//import tv.superawesome.lib.sajsonparser.SAJsonParser;
-//import tv.superawesome.lib.sasession.SASession;
-//import tv.superawesome.lib.sautils.SAUtils;
-//import tv.superawesome.lib.samodelspace.SAVASTAd;
-//import tv.superawesome.lib.sautils.*;
-//import tv.superawesome.lib.samodelspace.SAAd;
-//import tv.superawesome.lib.samodelspace.SACreativeFormat;
-//import tv.superawesome.lib.samodelspace.SAData;
-//import tv.superawesome.lib.sanetwork.request.*;
-//
-///**
-// * This class gathers all the other parts of the "data" package and unifies the whole loading
-// * experience for the user
-// */
-//public class SALoader {
-//
-//    /**
-//     * the function that actually loads the Ad
-//     * @param placementId - the placement ID a user might want to preload an Ad for
-//     * @param listener - a reference to the listener
-//     */
-//    public void loadAd(final int placementId, final SALoaderInterface listener){
-//
-//        final String endpoint = SASession.getInstance().getBaseUrl() + "/ad/" + placementId;
-//
-//        SAUtils.SAConnectionType type = SAUtils.SAConnectionType.unknown;
-//        String packageName = "unknown";
-//        Context c = SAApplication.getSAApplicationContext();
-//        if (c != null) {
-//            type = SAUtils.getNetworkConnectivity(c);
-//            packageName = c.getPackageName();
-//
-//        }
-//
-//        JSONObject query = SAJsonParser.newObject(new Object[]{
-//                "test", SASession.getInstance().isTestEnabled(),
-//                "sdkVersion", SASession.getInstance().getVersion(),
-//                "rnd", SAUtils.getCacheBuster(),
-//                "bundle", packageName,
-//                "name", SAUtils.getAppLabel(),
-//                "dauid", SASession.getInstance().getDauId(),
-//                "ct", type.ordinal(),
-//                "lang", Locale.getDefault().toString(),
-//                "device", SAUtils.getSystemSize() == SAUtils.SASystemSize.mobile ? "mobile" : "tablet"
-//        });
-//
-//        JSONObject header = SAJsonParser.newObject(new Object[]{
-//                "Content-Type", "application/json",
-//                "User-Agent", SAUtils.getUserAgent()
-//        });
-//
-//        SANetwork network = new SANetwork();
-//        network.sendGET(c, endpoint, query, header, new SANetworkInterface() {
-//            @Override
-//            public void response(int status, String data, boolean success) {
-//                if (!success || data == null) {
-//                    failAd(listener, placementId);
-//                    return;
-//                }
-//
-//                // get data
-//                JSONObject dataJson = SAJsonParser.newObject(data);
-//                final SAAd ad = SAParser.parseDictionaryIntoAd(dataJson, placementId);
-//
-//                if (ad != null) {
-//
-//                    Log.d("SuperAwesome", "Ad data is: " + ad.isValid() + " | " + data);
-//
-//                    ad.creative.details.data = new SAData();
-//                    SACreativeFormat type = ad.creative.creativeFormat;
-//
-//                    switch (type) {
-//                        case invalid:
-//                        case image:
-//                        case rich:
-//                        case tag: {
-//                            ad.creative.details.data.adHtml = SAHTMLParser.formatCreativeDataIntoAdHTML(ad);
-//                            didLoadAd(listener, ad);
-//                            break;
-//                        }
-//                        case video: {
-//                            SAVASTParser parser = new SAVASTParser();
-//
-//                            parser.parseVASTAds(ad.creative.details.vast, new SAVASTParserInterface() {
-//                                @Override
-//                                public void didParseVAST(SAVASTAd vastAd) {
-//                                    if (vastAd != null) {
-//                                        ad.creative.details.data.vastAd = vastAd;
-//                                        didLoadAd(listener, ad);
-//                                    } else {
-//                                        failAd(listener, ad.placementId);
-//                                    }
-//                                }
-//                            });
-//                            break;
-//                        }
-//                    }
-//                } else {
-//                    failAd(listener, placementId);
-//                }
-//            }
-//        });
-//    }
-//
-//    void didLoadAd(SALoaderInterface listener, SAAd ad) {
-//        if (listener != null) {
-//            listener.didLoadAd(ad);
-//        }
-//    }
-//
-//    private void failAd(SALoaderInterface listener, int placementId){
-//        if (listener != null){
-//            listener.didFailToLoadAdForPlacementId(placementId);
-//        }
-//    }
-//}
+/**
+ * @class: SALoader.java
+ * @copyright: (c) 2015 SuperAwesome Ltd. All rights reserved.
+ * @author: Gabriel Coman
+ * @date: 28/09/2015
+ *
+ */
+package tv.superawesome.lib.saadloader;
+
+/**
+ * Imports needed for this implementation
+ */
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.Locale;
+
+import tv.superawesome.lib.sajsonparser.SAJsonParser;
+import tv.superawesome.lib.samodelspace.SAMedia;
+import tv.superawesome.lib.sasession.SASession;
+import tv.superawesome.lib.sautils.SAUtils;
+import tv.superawesome.lib.sautils.*;
+import tv.superawesome.lib.samodelspace.SAAd;
+import tv.superawesome.lib.samodelspace.SACreativeFormat;
+import tv.superawesome.lib.sanetwork.request.*;
+
+/**
+ * This class gathers all the other parts of the "data" package and unifies the whole loading
+ * experience for the user
+ */
+public class SALoader {
+
+    /**
+     * the function that actually loads the Ad
+     * @param placementId - the placement ID a user might want to preload an Ad for
+     * @param listener - a reference to the listener
+     */
+    public void loadAd(final int placementId, final SALoaderInterface listener){
+
+        // create a local listener to avoid the "chain of listenerrrrs"
+        final SALoaderInterface localListener = listener != null ? listener : new SALoaderInterface() { @Override public void didLoadAd(SAAd ad) {} };
+
+        // form the endpoint
+        final String endpoint = SASession.getInstance().getBaseUrl() + "/ad/" + placementId;
+
+        SAUtils.SAConnectionType type = SAUtils.SAConnectionType.unknown;
+        String packageName = "unknown";
+        Context c = SAApplication.getSAApplicationContext();
+        if (c != null) {
+            type = SAUtils.getNetworkConnectivity(c);
+            packageName = c.getPackageName();
+        }
+
+        JSONObject query = SAJsonParser.newObject(new Object[]{
+                "test", SASession.getInstance().isTestEnabled(),
+                "sdkVersion", SASession.getInstance().getVersion(),
+                "rnd", SAUtils.getCacheBuster(),
+                "bundle", packageName,
+                "name", SAUtils.getAppLabel(),
+                "dauid", SASession.getInstance().getDauId(),
+                "ct", type.ordinal(),
+                "lang", Locale.getDefault().toString(),
+                "device", SAUtils.getSystemSize() == SAUtils.SASystemSize.mobile ? "mobile" : "tablet"
+        });
+
+        JSONObject header = SAJsonParser.newObject(new Object[]{
+                "Content-Type", "application/json",
+                "User-Agent", SAUtils.getUserAgent()
+        });
+
+        SANetwork network = new SANetwork();
+        network.sendGET(c, endpoint, query, header, new SANetworkInterface() {
+            @Override
+            public void response(int status, String data, boolean success) {
+                if (!success || data == null) {
+                    localListener.didLoadAd(null);
+                } else {
+                    // get data
+                    JSONObject dataJson = SAJsonParser.newObject(data);
+                    final SAAd ad = SAParser.parseInitialAdDataFromNetwork(dataJson, placementId);
+
+                    if (ad != null) {
+
+                        Log.d("SuperAwesome", "Ad data is: " + ad.isValid() + " | " + data);
+
+                        SACreativeFormat type = ad.creative.creativeFormat;
+
+                        switch (type) {
+                            case invalid:
+                            case image:
+                            case rich:
+                            case tag: {
+                                ad.creative.details.media = new SAMedia();
+                                ad.creative.details.media.html = SAHTMLParser.formatCreativeDataIntoAdHTML(ad);
+                                localListener.didLoadAd(ad);
+                                break;
+                            }
+                            case video: {
+                                SAVASTParser parser = new SAVASTParser();
+                                parser.parseVASTAds(ad.creative.details.vast, new SAVASTParserInterface() {
+                                    @Override
+                                    public void didParseVAST(SAAd vastAd) {
+                                        ad.sumAd(vastAd);
+                                        localListener.didLoadAd(ad);
+                                    }
+                                });
+                                break;
+                            }
+                        }
+                    } else {
+                        localListener.didLoadAd(null);
+                    }
+                }
+            }
+        });
+    }
+}
--- saadloader/src/main/java/tv/superawesome/lib/saadloader/SAParser.java
+++ saadloader/src/main/java/tv/superawesome/lib/saadloader/SAParser.java
@@ -1,184 +1,216 @@
-///**
-// * @class: SAParser.java
-// * @copyright: (c) 2015 SuperAwesome Ltd. All rights reserved.
-// * @author: Gabriel Coman
-// * @date: 29/10/2015
-// *
-// */
-//package tv.superawesome.lib.saadloader;
-//
-//import android.content.Context;
-//import android.util.Log;
-//
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//import java.util.ConcurrentModificationException;
-//
-//import tv.superawesome.lib.sajsonparser.SAJsonParser;
-//import tv.superawesome.lib.samodelspace.SACampaignType;
-//import tv.superawesome.lib.sasession.SASession;
-//import tv.superawesome.lib.sautils.*;
-//import tv.superawesome.lib.samodelspace.SAAd;
-//import tv.superawesome.lib.samodelspace.SACreativeFormat;
-//
-///**
-// * This is a class of static functions that make it easy to parse Ad responses from the
-// * server into SuperAwesome SDK models
-// */
-//public class SAParser {
-//
-//    /**
-//     * Parses a dictionary received from the server into a valid Ad object
-//     * @param dict - the dictionary to parse
-//     * @param placementId = the placement id - just used because it's not returned by the ad server
-//     */
-//    public static SAAd parseDictionaryIntoAd(JSONObject dict, int placementId) {
-//
-//        SAUtils.SAConnectionType ct = SAUtils.SAConnectionType.unknown;
-//        Context c = SAApplication.getSAApplicationContext();
-//        if (c != null) ct = SAUtils.getNetworkConnectivity(c);
-//
-//        /** surround with a try catch block */
-//        try {
-//            SAAd ad = new SAAd(dict);
-//            ad.placementId = placementId;
-//
-//            /** prform the next steps of the parsing */
-//            ad.creative.creativeFormat = SACreativeFormat.invalid;
-//            if (ad.creative.format.equals("image_with_link")) ad.creative.creativeFormat = SACreativeFormat.image;
-//            else if (ad.creative.format.equals("video")) ad.creative.creativeFormat = SACreativeFormat.video;
-//            else if (ad.creative.format.contains("rich_media")) ad.creative.creativeFormat = SACreativeFormat.rich;
-//            else if (ad.creative.format.contains("tag")) ad.creative.creativeFormat = SACreativeFormat.tag;
-//
-//            /** cpm vs cpi */
+/**
+ * @class: SAParser.java
+ * @copyright: (c) 2015 SuperAwesome Ltd. All rights reserved.
+ * @author: Gabriel Coman
+ * @date: 29/10/2015
+ *
+ */
+package tv.superawesome.lib.saadloader;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ConcurrentModificationException;
+
+import tv.superawesome.lib.sajsonparser.SAJsonParser;
+import tv.superawesome.lib.samodelspace.SACampaignType;
+import tv.superawesome.lib.samodelspace.SATracking;
+import tv.superawesome.lib.sasession.SASession;
+import tv.superawesome.lib.sautils.*;
+import tv.superawesome.lib.samodelspace.SAAd;
+import tv.superawesome.lib.samodelspace.SACreativeFormat;
+
+/**
+ * This is a class of static functions that make it easy to parse Ad responses from the
+ * server into SuperAwesome SDK models
+ */
+public class SAParser {
+
+    /**
+     * Parses a dictionary received from the server into a valid Ad object
+     * @param dict - the dictionary to parse
+     * @param placementId = the placement id - just used because it's not returned by the ad server
+     */
+    public static SAAd parseInitialAdDataFromNetwork(JSONObject dict, int placementId) {
+
+        SAUtils.SAConnectionType ct = SAUtils.SAConnectionType.unknown;
+        Context c = SAApplication.getSAApplicationContext();
+        if (c != null) ct = SAUtils.getNetworkConnectivity(c);
+
+        /** surround with a try catch block */
+        try {
+            SAAd ad = new SAAd(dict);
+            ad.placementId = placementId;
+
+            /** prform the next steps of the parsing */
+            ad.creative.creativeFormat = SACreativeFormat.invalid;
+            if (ad.creative.format.equals("image_with_link")) ad.creative.creativeFormat = SACreativeFormat.image;
+            else if (ad.creative.format.equals("video")) ad.creative.creativeFormat = SACreativeFormat.video;
+            else if (ad.creative.format.contains("rich_media")) ad.creative.creativeFormat = SACreativeFormat.rich;
+            else if (ad.creative.format.contains("tag")) ad.creative.creativeFormat = SACreativeFormat.tag;
+
+            /** cpm vs cpi */
 //            ad.saCampaignType = SACampaignType.CPM;
 //            if (ad.campaignType == 1) ad.saCampaignType = SACampaignType.CPI;
-//
-//
-//            JSONObject trackingDict = SAJsonParser.newObject(new Object[]{
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal()
-//            });
-//
-//            ad.creative.trackingUrl = SASession.getInstance().getBaseUrl() +
-//                    (ad.creative.creativeFormat == SACreativeFormat.video ? "/video/click/?" : "/click?") +
-//                    SAUtils.formGetQueryFromDict(trackingDict);
-//
-//            JSONObject impressionDict1 = SAJsonParser.newObject(new Object[]{
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "type", "viewable_impression"
-//            });
-//
-//            JSONObject impressionDict2 = SAJsonParser.newObject(new Object[]{
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal(),
-//                    "data", SAUtils.encodeDictAsJsonDict(impressionDict1)
-//            });
-//
-//            ad.creative.viewableImpressionUrl = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(impressionDict2);
-//
-//            /** create the parental gate URLs */
-//            JSONObject pgcloseDict1 = SAJsonParser.newObject(new Object[]{
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "type", "parentalGateClose"
-//            });
-//
-//            JSONObject pgcloseDict2 = SAJsonParser.newObject(new Object[]{
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal(),
-//                    "data", SAUtils.encodeDictAsJsonDict(pgcloseDict1)
-//            });
-//
-//            ad.creative.parentalGateCloseUrl = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgcloseDict2);
-//
-//            JSONObject pgopenDict1 = SAJsonParser.newObject(new Object[]{
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "type", "parentalGateOpen"
-//            });
-//
-//            JSONObject pgopenDict2 = SAJsonParser.newObject(new Object[]{
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal(),
-//                    "data", SAUtils.encodeDictAsJsonDict(pgopenDict1)
-//            });
-//
-//            ad.creative.parentalGateOpenUrl = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgopenDict2);
-//
-//            JSONObject pgfailDict1 = SAJsonParser.newObject(new Object[]{
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "type", "parentalGateFail"
-//            });
-//
-//
-//            JSONObject pgfailDict2 = SAJsonParser.newObject(new Object[]{
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal(),
-//                    "data", SAUtils.encodeDictAsJsonDict(pgfailDict1)
-//            });
-//
-//            ad.creative.parentalGateFailUrl = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgfailDict2);
-//
-//            JSONObject pgsuccessDict1 = SAJsonParser.newObject(new Object[] {
-//                    "placement", ad.placementId,
-//                    "line_item", ad.lineItemId,
-//                    "creative", ad.creative.id,
-//                    "type", "parentalGateSuccess"
-//            });
-//
-//            JSONObject pgsuccessDict2 = SAJsonParser.newObject(new Object[] {
-//                    "sdkVersion", SASession.getInstance().getVersion(),
-//                    "rnd", SAUtils.getCacheBuster(),
-//                    "ct", ct.ordinal(),
-//                    "data", SAUtils.encodeDictAsJsonDict(pgsuccessDict1)
-//            });
-//
-//            ad.creative.parentalGateSuccessUrl = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgsuccessDict2);
-//
-//            // cdn url
-//            switch (ad.creative.creativeFormat) {
-//                case tag:
-//                case invalid: { break; }
-//                case image: {
-//                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.image);
-//                    break;
-//                }
-//                case video: {
-//                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.video);
-//                    break;
-//                }
-//                case rich: {
-//                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.url);
-//                    break;
-//                }
-//            }
-//
-//            /** do a validity check */
-//            if (!ad.isValid()) {
-//                return null;
-//            }
-//
-//            /** return proper ad */
-//            return ad;
-//        } catch (Exception e){
-//            return null;
-//        }
-//    }
-//
-//}
+
+            JSONObject trackingDict = SAJsonParser.newObject(new Object[]{
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal()
+            });
+
+            SATracking trackingEvt = new SATracking();
+            trackingEvt.event = "sa_tracking";
+            trackingEvt.URL = SASession.getInstance().getBaseUrl() +
+                    (ad.creative.creativeFormat == SACreativeFormat.video ? "/video/click/?" : "/click?") +
+                    SAUtils.formGetQueryFromDict(trackingDict);
+
+            SATracking impr = new SATracking();
+            if (ad.creative.impressionUrl != null) {
+                impr.URL = ad.creative.impressionUrl;
+                impr.event = "impression";
+            }
+
+
+            JSONObject impressionDict1 = SAJsonParser.newObject(new Object[]{
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "type", "viewable_impression"
+            });
+
+            JSONObject impressionDict2 = SAJsonParser.newObject(new Object[]{
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal(),
+                    "data", SAUtils.encodeDictAsJsonDict(impressionDict1)
+            });
+
+            SATracking viewableImpression = new SATracking();
+            viewableImpression.event = "viewable_impr";
+            viewableImpression.URL = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(impressionDict2);
+
+
+            /** create the parental gate URLs */
+            JSONObject pgcloseDict1 = SAJsonParser.newObject(new Object[]{
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "type", "parentalGateClose"
+            });
+
+            JSONObject pgcloseDict2 = SAJsonParser.newObject(new Object[]{
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal(),
+                    "data", SAUtils.encodeDictAsJsonDict(pgcloseDict1)
+            });
+
+            SATracking parentalGateClose = new SATracking();
+            parentalGateClose.event = "pg_close";
+            parentalGateClose.URL = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgcloseDict2);
+
+
+            JSONObject pgopenDict1 = SAJsonParser.newObject(new Object[]{
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "type", "parentalGateOpen"
+            });
+
+            JSONObject pgopenDict2 = SAJsonParser.newObject(new Object[]{
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal(),
+                    "data", SAUtils.encodeDictAsJsonDict(pgopenDict1)
+            });
+
+            SATracking parentalGateOpen = new SATracking();
+            parentalGateOpen.event = "pg_open";
+            parentalGateOpen.URL = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgopenDict2);
+
+
+            JSONObject pgfailDict1 = SAJsonParser.newObject(new Object[]{
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "type", "parentalGateFail"
+            });
+
+            JSONObject pgfailDict2 = SAJsonParser.newObject(new Object[]{
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal(),
+                    "data", SAUtils.encodeDictAsJsonDict(pgfailDict1)
+            });
+
+            SATracking parentalGateFail = new SATracking();
+            parentalGateFail.event = "pg_fail";
+            parentalGateFail.URL = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgfailDict2);
+
+
+            JSONObject pgsuccessDict1 = SAJsonParser.newObject(new Object[] {
+                    "placement", ad.placementId,
+                    "line_item", ad.lineItemId,
+                    "creative", ad.creative.id,
+                    "type", "parentalGateSuccess"
+            });
+
+            JSONObject pgsuccessDict2 = SAJsonParser.newObject(new Object[] {
+                    "sdkVersion", SASession.getInstance().getVersion(),
+                    "rnd", SAUtils.getCacheBuster(),
+                    "ct", ct.ordinal(),
+                    "data", SAUtils.encodeDictAsJsonDict(pgsuccessDict1)
+            });
+
+            SATracking parentalGateSuccess = new SATracking();
+            parentalGateSuccess.event = "pg_success";
+            parentalGateSuccess.URL = SASession.getInstance().getBaseUrl() + "/event?" + SAUtils.formGetQueryFromDict(pgsuccessDict2);
+
+            ad.creative.events.add(trackingEvt);
+            ad.creative.events.add(viewableImpression);
+            ad.creative.events.add(parentalGateClose);
+            ad.creative.events.add(parentalGateFail);
+            ad.creative.events.add(parentalGateOpen);
+            ad.creative.events.add(parentalGateSuccess);
+            if (ad.creative.impressionUrl != null) {
+                ad.creative.events.add(impr);
+            }
+
+            // cdn url
+            switch (ad.creative.creativeFormat) {
+                case tag:
+                case invalid: { break; }
+                case image: {
+                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.image);
+                    break;
+                }
+                case video: {
+                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.video);
+                    break;
+                }
+                case rich: {
+                    ad.creative.details.cdnUrl = SAUtils.findBaseURLFromResourceURL(ad.creative.details.url);
+                    break;
+                }
+            }
+
+            /** do a validity check */
+            if (!ad.isValid()) {
+                return null;
+            }
+
+            /** return proper ad */
+            return ad;
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+}
