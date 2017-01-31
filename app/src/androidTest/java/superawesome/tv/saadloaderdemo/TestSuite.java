f48b5405291fc9f55504478840d4c7840b15c72c
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-01-31 12:35:17 +0000

    Changes

--- app/src/androidTest/java/superawesome/tv/saadloaderdemo/TestSuite.java
+++ app/src/androidTest/java/superawesome/tv/saadloaderdemo/TestSuite.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        SAAdLoader_Async_Tests.class,
+//        SAAdLoader_Async_Tests.class,
+        SAAdLoader_Async_Tests2.class,
         SAAdLoader_Endpoint_Tests.class,
         SAAdLoader_ProcessEvents_Tests.class,
         SAAdLoader_ProcessHTML_Tests.class
